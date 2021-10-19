599cda061bf4e5fe1f6c71292aae023dd56e98e2
Julien Orjollet <oj.professionnel@gmail.com>
2021-10-19 11:37:50 +0200

    removed sticky header script and animation

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,11 @@
 import { getNewYear } from "./helper/getNewYear.js"
 import { getFlexboxGap } from "./helper/getFlexboxGap.js"
 import { mobileMenu } from "./modules/mobileMenu.js"
-import { stickyHeader } from "./modules/stickyHeader.js"
 import { parallaxEffect } from "./modules/parallaxEffect.js"
 import { toggle } from "./modules/toggle.js"
 import { showDashboardLogin } from "./modules/showDashboardLogin.js"
 
 function init() {
-  stickyHeader()
   mobileMenu()
   parallaxEffect()
   getNewYear()
