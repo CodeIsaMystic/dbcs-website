a645ae7913ba636aae0f93d6ff666d723da7467c
Julien Orjollet <oj.professionnel@gmail.com>
2021-10-19 12:47:59 +0200

    cleaning some styles

--- public/js/modules/mobileMenu.js
+++ public/js/modules/mobileMenu.js
@@ -1,12 +1,10 @@
 const mobileMenu = function () {
   const menuIcon = document.querySelector(".menu-icon")
   const primaryNav = document.querySelector(".header .primary-nav")
-  // const siteHeader = document.querySelector(".header")
 
   function toggleTheMenu() {
     primaryNav.classList.toggle("primary-nav--is-visible")
     menuIcon.classList.toggle("menu-icon--close-x")
-    // siteHeader.classList.toggle("header--is-expanded")
   }
 
   menuIcon.addEventListener("click", () => toggleTheMenu())
